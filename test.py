38bdb0c41eae2d26b4c888505ab0afb67daaa411
shangyindong <shangyindong@p1.com>
2021-09-30 17:03:35 +0800

    c

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import tensorflow as tf
-from collections import deque
-
-q = deque()
-q.append([1,2,3])
-while q:
-    d,e,f = q.popleft()
-    print(d)
---
+++ tf/linear_problem.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import Model, layers, optimizers
+
+X = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+y = tf.constant([[10.0], [20.0]])
+
+
+class Linear(Model):
+    def __init__(self):
+        super().__init__()
+        self.dense = layers.Dense(
+            units=1,  # 输出张量纬度
+            activation=None,
+            kernel_initializer=tf.zeros_initializer(),
+            bias_initializer=tf.zeros_initializer()
+        )
+
+    def call(self, input):
+        output = self.dense(input)
+        return output
+
+
+model = Linear()
+optimizer = optimizers.SGD(learning_rate=0.01)
+for i in range(100):
+    with tf.GradientTape()as tape:
+        y_pred = model(X)
+        loss = tf.reduce_sum(tf.square(y_pred - y))
+    grads = tape.gradient(loss, model.variables)
+    optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
+
+print(model.variables)
